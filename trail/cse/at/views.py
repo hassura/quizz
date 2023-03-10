1ebd419dce01ef1807c12e0b1ad1ded12c3f5a22
hassura <126978351+hassura@users.noreply.github.com>
2023-03-11 01:22:59 +0530

    w
    w

--- trail/cse/at/views.py
+++ trail/cse/at/views.py
@@ -2,6 +2,6 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return render(request,"at/index.html")
+    return render(request,"cse/template/at/index.html")
 
 # Create your views here.

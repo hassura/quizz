07ebc043303336ef1c6789822e5fb89042996af8
hassura <126978351+hassura@users.noreply.github.com>
2023-04-12 00:02:28 +0530

    dsv
    rf

--- trail/cse/at/views.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing import context
-from django.shortcuts import render
-from django.http import HttpResponse
-def home(request):
-    return render(request,"cse/template/at/index.html")
-
-# Create your views here.
